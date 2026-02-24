acb697db4e4c4507603b4188a61434aeb375fca4
Arpita islam kona <arpitaislamkona21@gmail.com>
2026-02-24 15:49:23 +0200

    job card

---
+++ script.js
@@ -0,0 +1,10 @@
+const jobs = [
+  {id:1, company:"Mobile First Corp", position:"React Native Developer", location:"Remote", salary:"80,000 - 120,000", description:"Build cross platform mobile applications.", status:"all"},
+  {id:2, company:"WebFlow Agency", position:"UI Designer", location:"Dhaka", salary:"50,000 - 80,000", description:"Design modern UI for web applications.", status:"all"},
+  {id:3, company:"DataVolt Solutions", position:"Data Specialist", location:"Chattogram", salary:"60,000 - 95,000", description:"Work with data analytics and reporting.", status:"all"},
+  {id:4, company:"CloudNest Inc", position:"Backend Engineer", location:"Remote", salary:"70,000 - 110,000", description:"Develop scalable backend systems.", status:"all"},
+  {id:5, company:"Innovative Lab", position:"AI Engineer", location:"Dhaka", salary:"90,000 - 150,000", description:"Develop AI based applications.", status:"all"},
+  {id:6, company:"MegaCore Solutions", position:"Frontend Engineer", location:"Khulna", salary:"55,000 - 85,000", description:"Develop modern frontend UI.", status:"all"},
+  {id:7, company:"StarbyteX", position:"Full Stack Engineer", location:"Rajshahi", salary:"65,000 - 100,000", description:"Work on MERN stack projects.", status:"all"},
+  {id:8, company:"TechCorp Industries", position:"DevOps Engineer", location:"Dhaka", salary:"85,000 - 130,000", description:"Maintain CI/CD pipelines.", status:"all"},
+];
